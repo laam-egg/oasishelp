db78f9f86574b45eef9c2e0e095dccd9729d6c0b
Vu Tung Lam <vutunglaminfo@gmail.com>
2023-11-02 12:17:23 +0700

    fix: prettier-java-plugin cannot be imported on Vercel host

--- src/app/api/prettify/java/route.ts
+++ src/app/api/prettify/java/route.ts
@@ -1,4 +1,6 @@
 import * as prettier from 'prettier';
+import * as prettierPluginJava from '@/lib/prettierPluginJava';
+
 import { NextResponse, type NextRequest } from 'next/server';
 
 export async function POST(req: NextRequest) {
@@ -8,7 +10,7 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({
             code: await prettier.format(code, {
                 parser: 'java',
-                plugins: [ 'prettier-plugin-java' ],
+                plugins: [ prettierPluginJava ],
                 tabWidth: 4,
                 useTabs: false,
             }),
