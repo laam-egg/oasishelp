75715d5e22104169649bab259f2e359221a8347a
Vu Tung Lam <vutunglaminfo@gmail.com>
2023-11-04 00:43:22 +0700

    feat: add GitHub repo link into homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <OutputCodeEditor />
+      <br />
+      <div><a href='https://github.com/laam-egg/oasishelp' target='_blank'>View source code on GitHub.</a></div>
     </main>
   )
 }
